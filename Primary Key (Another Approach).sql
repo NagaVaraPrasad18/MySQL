f9f5f68729855812db541bd080d6d4311458b443
NagaVaraPrasad18 <nagamallinagavaraprasad@gmail.com>
2024-07-23 23:45:27 +0530

    23 July Notes

---
+++ Primary Key (Another Approach).sql
@@ -0,0 +1,16 @@
+-- Primary Key (Another Approach)
+
+create table cats4 (
+SNo int,
+primary key (SNo),
+name varchar(20) not null,
+age int not null
+);
+
+insert into cats4 (SNo, name, age)
+values (1, 'name1', 1),
+	   (2, 'name2', 2);
+
+desc cats4;
+
+select * from cats4;
